c4ed0bfa5f29a6e433472d1b5064e034b3076ba2
fullpower <185908071@qq.com>
2022-03-12 21:03:43 +0800

    part c

--- a3/parser_transitions.py
+++ a3/parser_transitions.py
@@ -32,7 +32,9 @@ class PartialParse(object):
         ### Note: The root token should be represented with the string "ROOT"
         ### Note: If you need to use the sentence object to initialize anything, make sure to not directly 
         ###       reference the sentence object.  That is, remember to NOT modify the sentence object. 
-
+        self.stack = ["ROOT"]
+        self.buffer = sentence.copy()
+        self.dependencies = []
 
         ### END YOUR CODE
 
@@ -51,7 +53,16 @@ class PartialParse(object):
         ###         1. Shift
         ###         2. Left Arc
         ###         3. Right Arc
-
+        if transition == "S":
+            self.stack.append(self.buffer.pop(0))
+        elif transition == "LA":
+            dependent = self.stack.pop(-2)
+            self.dependencies.append((self.stack[-1], dependent))
+        elif transition == "RA":
+            dependent = self.stack.pop(-1)
+            self.dependencies.append((self.stack[-1], dependent))
+        else:
+            raise Exception("Invalid Transition")
 
         ### END YOUR CODE
 
