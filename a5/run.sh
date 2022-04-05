e388b2cf7abc033a719add997e9721a1091a1ce0
fullpower <185908071@qq.com>
2022-04-05 22:50:28 +0800

    run.sh

---
+++ a5/run.sh
@@ -0,0 +1,16 @@
+# Train on the names dataset
+python3 src/run.py finetune vanilla wiki.txt \
+    --writing_params_path vanilla.model.params \
+    --finetune_corpus_path birth_places_train.tsv
+
+# Evaluate on the dev set, writing out predictions
+python3 src/run.py evaluate vanilla wiki.txt \
+    --reading_params_path vanilla.model.params \
+    --eval_corpus_path birth_dev.tsv \
+    --outputs_path vanilla.nopretrain.dev.predictions
+
+# Evaluate on the test set, writing out predictions
+python3 src/run.py evaluate vanilla wiki.txt \
+    --reading_params_path vanilla.model.params \
+    --eval_corpus_path birth_test_inputs.tsv \
+    --outputs_path vanilla.nopretrain.test.prediction
